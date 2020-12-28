2ebae70c23a99397d760c01734fe71227b8c2fb0
trongthongvo <56016681+trongthongvo@users.noreply.github.com>
2020-12-28 16:03:50 +0700

    Update build.sh

--- pipeline6/jenkins/build/build.sh
+++ pipeline6/jenkins/build/build.sh
@@ -3,7 +3,7 @@
 # Copy the new jar to the build location
 cp -f pipeline6/java-app/target/*.jar pipeline6/jenkins/build/
 echo "copied"
-ll -la pipeline6/jenkins/build/
+ls -la pipeline6/jenkins/build/
 
 echo "****************************"
 echo "** Building Docker Image ***"
