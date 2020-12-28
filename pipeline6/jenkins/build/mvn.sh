4054d107b00eb6a8d422941e9b498192b739e236
trongthongvo <56016681+trongthongvo@users.noreply.github.com>
2020-12-28 15:11:04 +0700

    Update mvn.sh

--- pipeline6/jenkins/build/mvn.sh
+++ pipeline6/jenkins/build/mvn.sh
@@ -7,5 +7,7 @@ echo "***************************"
 #WORKSPACE=/home/centos/mypipeline/jenkins-resources/jenkins/pipeline/jenkins_home/workspace/pipeline-maven-docker
 #WORKSPACE=/home/centos/mypipeline/jenkins-resources/jenkins/pipeline/jenkins_home/workspace/pipeline-git
 echo $WORKSPACE
+ll $WORKSPACE
+pwd
 
 docker run --rm  -v  $WORKSPACE/pipeline6/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
